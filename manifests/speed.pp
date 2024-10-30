36ddca2f1cb423d1dea8648e43f4d76d4f08b7f9
Code Manager <code_manager@rocky8-330>
2024-10-30 14:31:22 +0000

    Fact override in test throwing warning.

---
+++ manifests/speed.pp
@@ -0,0 +1,8 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include test_330::speed
+class test_330::speed {
+}
---
+++ spec/classes/speed_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'test_330::speed' do
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts.merge(networking:{interfaces: { eth0: { bindings: [{address: '10.10.10.10'},]}}}) }
+      it { is_expected.to compile.with_all_deps }
+    end
+  end
+end
